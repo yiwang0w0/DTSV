974d81cdc8883dcc49c2f890d6d200a01f5222aa
yiwang0w0 <77381968+yiwang0w0@users.noreply.github.com>
2025-07-15 15:09:25 +0900

    fix item pickup workflow

--- backend/src/utils/item.js
+++ backend/src/utils/item.js
@@ -10,6 +10,26 @@ async function dropMapItem(pls, name, kind, effect, uses, skill, session) {
   }
 }
 
+async function restoreMemoryItem(player) {
+  if (!player.searchmemory) return;
+  try {
+    const item = JSON.parse(player.searchmemory);
+    const data = {
+      itm: item.itm,
+      itmk: item.itmk,
+      itme: item.itme,
+      itms: item.itms,
+      itmsk: item.itmsk,
+      pls: item.pls
+    };
+    await MapItem.create(data);
+  } catch (e) {
+    console.error(e);
+  }
+  player.searchmemory = '';
+}
+
 module.exports = {
-  dropMapItem
+  dropMapItem,
+  restoreMemoryItem
 };
