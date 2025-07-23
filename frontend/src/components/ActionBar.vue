1b343485ac39ab9a846ec0b6026123073d64fdd1
yiwang0w0 <77381968+yiwang0w0@users.noreply.github.com>
2025-07-23 17:01:17 +0900

    feat: add move button and adjust sp cost

--- frontend/src/components/ActionBar.vue
+++ frontend/src/components/ActionBar.vue
@@ -3,6 +3,7 @@
     <el-select v-model="val" placeholder="选择地点" style="width: 180px" @change="handleChange">
       <el-option v-for="(n, i) in places" :key="i" :label="n" :value="i" />
     </el-select>
+    <el-button @click="$emit('move')">移动</el-button>
     <el-button @click="$emit('search')">搜索</el-button>
     <el-button @click="$emit('rest')">休息</el-button>
   </div>
@@ -14,7 +15,7 @@ const props = defineProps({
   modelValue: Number,
   places: { type: Array, default: () => [] }
 })
-const emit = defineEmits(['update:modelValue','change','search','rest'])
+const emit = defineEmits(['update:modelValue','change','move','search','rest'])
 const val = computed({
   get: () => props.modelValue,
   set: v => emit('update:modelValue', v)
